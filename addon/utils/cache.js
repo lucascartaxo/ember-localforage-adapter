26164e776c5da66c3c146e8d7b6333ce2eb4cb77
Lucas Cartaxo <lucas.rolim@digitalpages.com.br>
2015-07-16 18:54:17 -0300

    remove Ember.A() of cache

--- addon/utils/cache.js
+++ addon/utils/cache.js
@@ -1,30 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Object.extend ({
-  
+
   data : Ember.Map.create(),
-  
+
   clear : function () {
     this.data.clear();
   },
-  
+
   get : function (namespace) {
     if (this.data.get(namespace)) {
-      return Ember.copy(Ember.A(this.data.get(namespace)), true);
+      return Ember.copy(this.data.get(namespace), true);
     } else {
       return null;
     }
   },
-  
+
   set : function (namespace, objects) {
-    this.data.set(namespace, Ember.copy(Ember.A(objects), true));
+    this.data.set(namespace, Ember.copy(objects, true));
   },
-  
+
   replace : function (data) {
     this.clear();
     for (var index in data) {
       this.set(index, data[index]);
     }
   }
-  
+
 });
